43852f72f56b7b82902262e0ceb1fd4d90de6af9
Kevin Allioli <kevin@stackops.ch>
2026-06-01 21:00:17 +0200

    feat(radius-user): expose vlan and tunnel_config_type
    Expose two fields supported by the go-unifi Account model: vlan (the VLAN
    assigned to the account, used for dynamic VLAN assignment) and
    tunnel_config_type (vpn|802.1x|custom). Includes an acceptance test with
    import verification.

--- unifi/radius_user_resource_test.go
+++ unifi/radius_user_resource_test.go
@@ -51,3 +51,40 @@ resource "unifi_radius_user" "test" {
 }
 `
 }
+
+func TestAccRadiusUser_vlan(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { preCheck(t) },
+		ProtoV6ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRadiusUserConfig_vlan(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("unifi_radius_user.vlan", "vlan", "100"),
+					resource.TestCheckResourceAttr(
+						"unifi_radius_user.vlan",
+						"tunnel_config_type",
+						"802.1x",
+					),
+				),
+			},
+			{
+				ResourceName:            "unifi_radius_user.vlan",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"password"},
+			},
+		},
+	})
+}
+
+func testAccRadiusUserConfig_vlan() string {
+	return `
+resource "unifi_radius_user" "vlan" {
+	name               = "test-account-vlan"
+	password           = "test-password"
+	vlan               = 100
+	tunnel_config_type = "802.1x"
+}
+`
+}
